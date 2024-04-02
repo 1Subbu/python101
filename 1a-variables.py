bfaa2659d002f811ce14962fa76d5b5f6fe5619c
VENKATESH THANNERU <venkateshthanneru55@gmail.com>
2024-04-02 16:43:12 +0000

    updated

---
+++ 1a-variables.py
@@ -0,0 +1,15 @@
+print(type(2))
+print(2%3)
+number = float(1)
+print(number)
+print(type(number))
+
+print(1+2)
+print(2-1)
+print(2/2)
+
+import math
+
+print(math.floor(5.2))
+
+print(math.ceil(7))
